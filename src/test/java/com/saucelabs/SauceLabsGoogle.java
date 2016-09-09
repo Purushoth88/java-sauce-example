71e6241901946e71bb80a75f002fc7a19f418539
Purushoth88 <purush_it2007@yahoo.co.in>
2016-09-09 11:35:34 +0530

    Update SauceLabsGoogle.java

--- src/test/java/com/saucelabs/SauceLabsGoogle.java
+++ src/test/java/com/saucelabs/SauceLabsGoogle.java
@@ -1,8 +1,21 @@
 package com.saucelabs;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import com.saucelabs.junit.Parallelized;
+import com.saucelabs.junit.ConcurrentParameterized;
+import com.saucelabs.junit.SauceOnDemandTestWatcher;
 import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
